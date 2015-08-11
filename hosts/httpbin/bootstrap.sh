b9eaaea72c552cd45c7d4b08c32cfc7b3ee96f56
Oliver Palmer <oliverpalmer@opalmer.com>
2015-08-11 17:17:51 -0400

    shouldn't need sudo

--- hosts/httpbin/bootstrap.sh
+++ hosts/httpbin/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash -ex
 
-sudo apt-get -y install python-dev uwsgi uwsgi-plugin-python nginx python-pip
-sudo pip install httpbin awscli
+apt-get -y install python-dev uwsgi uwsgi-plugin-python nginx python-pip
+pip install httpbin awscli
 
-sudo curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/nginx/sites-enabled/default -o /etc/nginx/sites-enabled/default
-sudo curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/uwsgi/apps-enabled/httpbin.ini -o /etc/uwsgi/apps-enabled/httpbin.ini
-sudo curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/ssl/certs/httpbin.pem /etc/ssl/certs/httpbin.pem
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/nginx/sites-enabled/default -o /etc/nginx/sites-enabled/default
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/uwsgi/apps-enabled/httpbin.ini -o /etc/uwsgi/apps-enabled/httpbin.ini
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/ssl/certs/httpbin.pem /etc/ssl/certs/httpbin.pem
 aws s3 cp s3://opalmer/aws/ssl/httpbin.key /etc/ssl/private/httpbin.key
