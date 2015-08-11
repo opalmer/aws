8d9ba348c9e83b846a25a3999a04f96bfa3e773e
Oliver Palmer <oliverpalmer@opalmer.com>
2015-08-11 17:18:50 -0400

    could have more than just /etc for files

--- hosts/httpbin/bootstrap.sh
+++ hosts/httpbin/bootstrap.sh
@@ -3,7 +3,7 @@
 apt-get -y install python-dev uwsgi uwsgi-plugin-python nginx python-pip
 pip install httpbin awscli
 
-curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/nginx/sites-enabled/default -o /etc/nginx/sites-enabled/default
-curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/uwsgi/apps-enabled/httpbin.ini -o /etc/uwsgi/apps-enabled/httpbin.ini
-curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/etc/ssl/certs/httpbin.pem /etc/ssl/certs/httpbin.pem
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/files/etc/nginx/sites-enabled/default -o /etc/nginx/sites-enabled/default
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/files/etc/uwsgi/apps-enabled/httpbin.ini -o /etc/uwsgi/apps-enabled/httpbin.ini
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/httpbin/files/etc/ssl/certs/httpbin.pem /etc/ssl/certs/httpbin.pem
 aws s3 cp s3://opalmer/aws/ssl/httpbin.key /etc/ssl/private/httpbin.key
