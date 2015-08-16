fff7ff580fab2ea15522992df5ebaf41daf8e4f9
Oliver Palmer <oliverpalmer@opalmer.com>
2015-08-16 11:38:48 -0400

    initial commit of stack

---
+++ hosts/build/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+
+# Configure hostname
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/pyfarm/files/etc/hostname -o /etc/hostname
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/pyfarm/files/etc/hosts -o /etc/hosts
+hostname -F /etc/hostname
+
+# Updates, core packages and dependencies
+apt-get update
+apt-get -y upgrade
+
+# Configure nginx
+rm -f /etc/nginx/sites-*/default
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/pyfarm/files/etc/nginx/sites-enabled/build -o /etc/nginx/sites-enabled/build
+[ -f /etc/ssl/dhparams.pem ] || openssl dhparam -out /etc/ssl/dhparams.pem 2048
+
+# SSL (public)
+curl https://raw.githubusercontent.com/opalmer/aws/master/hosts/pyfarm/files/etc/ssl/certs/build.pem -o /etc/ssl/certs/build.pem
+
+# SSL (private)
+pip install awscli
+aws s3 cp s3://opalmer/aws/ssl/build.key /etc/ssl/private/build.key
+
+# Restart services
+service nginx restart
+service buildmaster restart
+
+# Update Public IP addresses
+pip install boto
+curl https://raw.githubusercontent.com/opalmer/aws/master/scripts/aws_set_public_a_record -o /usr/bin/aws_set_public_a_record
+chmod +x /usr/bin/aws_set_public_a_record
+
+# TODO: change to real domain once done with development
+/usr/bin/aws_set_public_a_record betabuild.opalmer.com.
